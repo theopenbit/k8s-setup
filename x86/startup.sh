58497d350c7b930cc60d6e6806e4a38d30a44bdc
theOpenbit <tob@schoenesnetz.de>
2017-03-09 21:28:37 +0100

    added nfs server

--- x86/startup.sh
+++ x86/startup.sh
@@ -15,7 +15,8 @@ if [ $? -eq 1 ]; then
     curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
-
+sudo modprobe nfs
+sudo modprobe nfsd
 export KUBECONFIG=$1
 export KUBECONFIG_PATH=$(dirname $KUBECONFIG)
 sudo -E docker-compose up -d
