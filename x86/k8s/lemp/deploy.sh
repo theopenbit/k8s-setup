92e3162802b98c07dd1ec4777082ba7944b7531d
theOpenbit <tob@schoenesnetz.de>
2017-03-12 20:59:55 +0100

    wip

---
+++ x86/k8s/lemp/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -z $1 ] || [ -z $2 ]; then
+    echo 'usage deploy.sh <pathToYourKubeConfigFile> <namespace>'
+    exit 1
+fi
+
+docker run -v $(pwd):/k8swork -v $(dirname $1):$(dirname $1) -e KUBECONFIG=$1 k8sdeployer -n $2  -d /k8swork
+
