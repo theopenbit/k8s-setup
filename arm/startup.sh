623ebf100309054108b2c52cde8b982eaec20de9
theOpenbit <tob@schoenesnetz.de>
2017-03-14 19:52:13 +0100

    apt update

--- arm/startup.sh
+++ arm/startup.sh
@@ -15,6 +15,7 @@ if [ $? -eq 1 ]; then
     curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
+sudo apt update
 sudo apt install -y nfs-common
 mkdir -p nfsmnt
 chmod 777 nfsmnt
--- arm/worker_startup.sh
+++ arm/worker_startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #install additional packages on worker nodes
+sudo apt update
 sudo apt install -y nfs-common
