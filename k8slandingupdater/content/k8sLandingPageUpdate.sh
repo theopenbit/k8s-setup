db02f716fdf1f11ffddd80e2615d640359dc9985
theOpenBit <tob@schoenesnetz.de>
2017-04-25 16:42:39 +0000

    added parameter KUBEFQDN

--- k8slandingupdater/content/k8sLandingPageUpdate.sh
+++ k8slandingupdater/content/k8sLandingPageUpdate.sh
@@ -4,10 +4,17 @@ if [ -z $KUBECONFIG ]; then
     exit 1
 fi
 KUBERNETESMASTER=$(cat $KUBECONFIG | grep server | sed  -r "s/server.*:\/\/(.*):.*/\1/")
+if [ -z $KUBEFQDN ]; then 
+    echo KUBEFQDN not set using ip of kubernetes master
+    KUBEFQDN=$KUBERNETESMASTER    
+fi
+
+
+
 while true 
 do
     kubectl get svc --all-namespaces -o yaml > /tmp/svc.yaml
-    ruby ./svc2html.rb -i /tmp/svc.yaml -o /tmp/index.html -k $KUBERNETESMASTER 
+    ruby ./svc2html.rb -i /tmp/svc.yaml -o /tmp/index.html -k $KUBEFQDN 
     sleep 2
 done
 
